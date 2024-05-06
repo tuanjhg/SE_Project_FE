3330b408c45983ddd6de5b96c4b4e7cb5cb632ef
tuanjhg <tuankhongjhg902@gmail.com>
2024-05-06 15:46:39 +0700

    pass 3

--- src/firebase.config.js
+++ src/firebase.config.js
@@ -4,13 +4,13 @@ import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
- apiKey: "AIzaSyAghCnvHqSYpnKRUqixLky4XT9JYe77L0w",
-  authDomain: "seproject-1d64b.firebaseapp.com",
-  projectId: "seproject-1d64b",
-  storageBucket: "seproject-1d64b.appspot.com",
-  messagingSenderId: "579366870372",
-  appId: "1:579366870372:web:4105c257bf4d805552ee71",
-  measurementId: "G-HH6LRRYMP6"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
 };
 
 const app = getApps.length > 0 ? getApp() : initializeApp(firebaseConfig);

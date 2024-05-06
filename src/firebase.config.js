888538c11fb91235f1078c93be2db1fb9a787b43
tuanjhg <60817582+tuanjhg@users.noreply.github.com>
2024-05-06 15:20:37 +0700

    Update firebase.config.js

--- src/firebase.config.js
+++ src/firebase.config.js
@@ -4,14 +4,13 @@ import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
-  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-  databaseURL: process.env.REACT_APP_FIREBASE_DB_URL,
-  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_ID,
-  appId: process.env.REACT_APP_FIREBASE_APP_ID,
-  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
+ apiKey: "AIzaSyAghCnvHqSYpnKRUqixLky4XT9JYe77L0w",
+  authDomain: "seproject-1d64b.firebaseapp.com",
+  projectId: "seproject-1d64b",
+  storageBucket: "seproject-1d64b.appspot.com",
+  messagingSenderId: "579366870372",
+  appId: "1:579366870372:web:4105c257bf4d805552ee71",
+  measurementId: "G-HH6LRRYMP6"
 };
 
 const app = getApps.length > 0 ? getApp() : initializeApp(firebaseConfig);

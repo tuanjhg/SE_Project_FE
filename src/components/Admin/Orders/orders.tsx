114425fda8fead990c33b8895d9a2eee4ec45464
tuanjhg <tuankhongjhg902@gmail.com>
2024-05-12 12:11:32 +0700

    pass 20

--- src/components/Admin/Orders/orders.tsx
+++ src/components/Admin/Orders/orders.tsx
@@ -1,26 +1,44 @@
 import { MdDeleteForever } from "react-icons/md";
-import { Avatar } from "../../Assets";
-import {motion} from "framer-motion";
 import {BiEditAlt} from "react-icons/bi";
 import React from "react";
+import { useStateValue } from "../../../context/StateProvider";
 const Order = ({item}: {item:any}) => {
+  const [{checkoutData}, dispatch] = useStateValue();
   const order_date=Date.now();
-  const status = "Waiting for shipping"
+  const updateCheckoutData = (key:string, val:string) => {
+    dispatch({
+      type: "UPDATE_CHECKOUT_DATA",
+      checkoutData: {
+        ...checkoutData,
+        [key]:val
+      }
+    });
+  }
   return (
       <div className="max-w-sm h-auto rounded-lg border border-orange-50 bg-orange-600">
-        <div className="flex flex-col gap-1 items-center pb-10">
-          <motion.img
-              whileHover= {{scale:1.1}}
-              className="mb-3 w-24 h-24 rounded-full shadow-lg cursor-pointer object-contain"
-              src={item?.uid}
-              alt="avatar"
-          />
           <div className="flex flex-col items-center justify-center">
+          <span className="text-sm text-gray-300 ">{item?.uid}</span>
             <h5 className="mb-1 text-xl font-medium text-gray-900 dark:text-white">
               {item?.displayName }
             </h5>
             <span className="text-sm text-gray-300 ">{order_date}</span>
-            <span className="text-sm text-gray-300 ">{status}</span>
+            <label
+        htmlFor="provider"
+        className="font-bold text-sm mb-1 text-gray-100"
+      >
+        Select Status
+      </label>
+      <select
+        id="Status"
+        className="w-full px-3 py-2 mb-1 border-2 text-white border-gray-500 rounded-md focus:outline-none focus:border-orange-500 focus:text-orange-500 bg-cartItem transition-colors"
+        onChange={(e) => updateCheckoutData("Status", e.target.value)}
+      >
+        <option value="default" selected disabled>Waiting for shipping</option>
+        <option value="1">Waiting for shipping</option>
+        <option value="2">Done</option>
+        <option value="3">Cancel</option>
+
+      </select>
           </div>
           <div className="flex mt-4 space-x-3 lg:mt-6">
             <p
@@ -31,13 +49,12 @@ const Order = ({item}: {item:any}) => {
             </p>
             <p
                 className="inline-flex items-center py-2 px-4 text-xl font-medium text-center text-gray-900 shadow-lg bg-primary hover:bg-white rounded-lg cursor-pointer  "
-                title="Disable"
+                title="Confirm"
             >
               <BiEditAlt />
             </p>
           </div>
         </div>
-      </div>
   )
 }
 

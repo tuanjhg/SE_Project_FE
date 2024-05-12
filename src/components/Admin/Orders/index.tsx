e6b0ea8f93ea739b07bda0f17ff2574139a67845
tuanjhg <tuankhongjhg902@gmail.com>
2024-05-12 11:44:46 +0700

    pass 19

--- src/components/Admin/Orders/index.tsx
+++ src/components/Admin/Orders/index.tsx
@@ -5,49 +5,49 @@ import Order from "./orders";
 
 
 const Orders = () => {
-  const [{ orders }, dispatch] = useStateValue();
-  const [query, setQuery] = useState("");
-  const [filteredOrders, setFilteredOrders] = useState(orders);
-  
-  const filterOrders = () => {
-      if(query.length === 0) {
-        setFilteredOrders(orders);
-      }else{
-        const filter = orders.filter((item:any) => item.displayName.toLowerCase().includes(query.toLowerCase()));
-        setFilteredOrders(filter);
-      }
-  }
-  const searchOrders = (e: React.ChangeEvent<HTMLInputElement>) => {
-      setQuery(e.target.value);
-      filterOrders();
-  }
-  return (
-    <div className="w-full justify-center flex flex-col">
-      {/* search bar */}
-      <div className="w-full flex justify-center p-2 bg-white mb-4 rounded-lg">
-        <input
-          className="w-full p-2 outline-none rounded-lg "
-          type="text"
-          placeholder="Search order"
-          value={query}
-          onChange={(e) => searchOrders(e)}
-        />
-        {/* search button */}
-        <button className="flex items-center justify-center gap-3 text-orange-700 font-bold py-2 px-4 rounded-lg">
-          <FaSearch />
-        </button>
-      </div>
+    const [{ users }, dispatch] = useStateValue();
+    const [query, setQuery] = useState("");
+    const [filteredUsers, setFilteredUsers] = useState(users);
 
-      {/* dasboard statistics and counts */}
-      <div className="w-full grid grid-cols-3 gap-1">
-        {
-          filteredOrders.map((order:any) => (
-            <Order key={order.uid} item = {order} />
-          ))
+    const filterUsers = () => {
+        if(query.length === 0) {
+            setFilteredUsers(users);
+        }else{
+            const filter = users.filter((item:any) => item.displayName.toLowerCase().includes(query.toLowerCase()));
+            setFilteredUsers(filter);
         }
-      </div>
-    </div>
-  );
+    }
+    const searchUsers = (e: React.ChangeEvent<HTMLInputElement>) => {
+        setQuery(e.target.value);
+        filterUsers();
+    }
+    return (
+        <div className="w-full justify-center flex flex-col">
+            {/* search bar */}
+            <div className="w-full flex justify-center p-2 bg-white mb-4 rounded-lg">
+                <input
+                    className="w-full p-2 outline-none rounded-lg "
+                    type="text"
+                    placeholder="Search user"
+                    value={query}
+                    onChange={(e) => searchUsers(e)}
+                />
+                {/* search button */}
+                <button className="flex items-center justify-center gap-3 text-orange-700 font-bold py-2 px-4 rounded-lg">
+                    <FaSearch />
+                </button>
+            </div>
+
+            {/* dasboard statistics and counts */}
+            <div className="w-full grid grid-cols-3 gap-1">
+                {
+                    filteredUsers.map((user:any) => (
+                        <Order key={user.uid} item = {user} />
+                    ))
+                }
+            </div>
+        </div>
+    );
 };
 
 export default Orders;
